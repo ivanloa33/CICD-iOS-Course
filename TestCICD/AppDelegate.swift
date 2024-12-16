be810eaa2d074504f3461ae65fe2ed768f7c146b
Ivan Lopez <ivan.lopez>
2024-12-16 16:48:49 -0600

    Add AppDelegate

--- TestCICD/AppDelegate.swift
+++ TestCICD/AppDelegate.swift
@@ -5,4 +5,17 @@
 //  Created by Ivan Lopez on 16/12/24.
 //
 
-import Foundation
+import UIKit
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        AppCenter.start(withAppSecret: "17ae10e4-94a6-4863-b06f-3a3aaa8ec7fb",
+                        services: [
+                            Analytics.self,
+                            Crashes.self
+                        ])
+    }
+}
