c845b215d741997230209b8178fceadebf5b6a41
Ivan Lopez <ivan.lopez>
2024-12-16 16:31:49 -0600

    Initial Commit

---
+++ TestCICD/TestCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDApp.swift
+//  TestCICD
+//
+//  Created by Ivan Lopez on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
